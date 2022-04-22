5945295186742a45a16af57908bceffaa5a66348
Evely-27 <evelysantos27@gmail.com>
2022-04-22 20:03:03 -0300

    login,senha, e botão enviar

--- ProjetoTelas/App.tsx
+++ ProjetoTelas/App.tsx
@@ -1,20 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View,Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.background}>
+      <View style={styles.containerPerfil}> 
+        <Image style= {styles.containerPerfil}
+        source={require('./assets/fotoPerfil.jpg')}
+        />
+      </View>
+
+      <View style={styles.container}>
+        <TextInput
+          style= {styles.input}
+          placeholder='Email:'
+          autoCorrect={false}
+          onChangeText={()=> {}}
+          />
+
+          <TextInput
+          style= {styles.input}
+          placeholder="Senha:"
+          autoCorrect={false}
+          onChangeText={()=> {}}
+          />
+
+          <TouchableOpacity style={styles.btnEnviar}>
+            <Text>Entrar</Text>
+          </TouchableOpacity>
+
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  background:{
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'skyblue'
   },
+  containerPerfil:{
+    flex:1,
+    width:500, 
+    height:106, 
+    alignContent: 'center',
+
+  },
+  container:{
+    flex: 1,
+    alignItems:'center',
+    justifyContent: 'center',
+    width:'90%',
+    paddingBottom: 50
+  },
+  input:{
+    backgroundColor: '#FFF',
+    width: "90%",
+    marginBottom: 15,
+    color: '#black',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnEnviar:{
+    backgroundColor: '#35AAFF',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+  submitText:{
+    color:'#FFF',
+    fontSize:18,
+  },
+  btRegister:{
+    marginTop:10,
+  },
+  registerText:{
+    color: '#FFF'
+  }
 });
