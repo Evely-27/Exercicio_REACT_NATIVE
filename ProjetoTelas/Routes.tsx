6659917a81502137a8d8f0420c287a6064896b79
Evely-27 <evelysantos27@gmail.com>
2022-05-12 21:59:09 -0300

    criando componente Routes

---
+++ ProjetoTelas/Routes.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+// componente, sempre returna algo
+export default function Routes() {
+
+// criar uma constante para guardar a função de create navigation para utuliar o stack
+
+    const Stack = createStackNavigator();
+    return (
+        
+    );
+}
+
